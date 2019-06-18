94dfff03f1ff231e3f121a8e3394f9708a51c202
Jayden Oxley <jaydenoxley00@gmail.com>
2019-06-18 10:19:58 +1000

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Error = () => {
     return (
         <div>
-            <h1>Error! '<span className="errorLink">{`${window.location}`}</span>' is not recognised!</h1>
             <p>Please select a link or search for pictures.</p>
         </div>
     )
